803d4fca815023c40c9de506cf750f374d16c0f3
Chad King <cking24343@gmail.com>
2014-05-29 18:00:29 -0400

    Updated layouts and logic

--- src/king/chad/toaster/ToastIt.java
+++ src/king/chad/toaster/ToastIt.java
@@ -30,6 +30,7 @@ import king.chad.toaster.ToastHelper;
 
 public class ToastIt
 {
+	//--Variable declarations--
 	private Activity mActivity;
     private LayoutInflater mLayoutInflater;
     private LinearLayout mRootLayout;
@@ -37,13 +38,17 @@ public class ToastIt
     private ImageView mToastImage;
     private FontAwesomeText mToastIcon;
     private View mToastView;
+    private Toast t;
+    private ToastHelper toastHelper;
     private Pattern pattern;
     private Matcher matcher;
     private static Typeface font;
     private static Map<String, String> faMap;
-    
     private static final String HEX_PATTERN = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
 
+    /**
+	* Various toaster constructors to use:
+	*/
     @SuppressLint("ResourceAsColor")
 	public ToastIt(Activity activity)
 	{
@@ -52,16 +57,16 @@ public class ToastIt
 		}
 
 		 this.mActivity = activity;
+		 toastHelper = new ToastHelper(this.mActivity);
+		 t = new Toast(this.mActivity);
+		 
 	     mLayoutInflater = (LayoutInflater) this.mActivity.getLayoutInflater();
 	     mToastView = mLayoutInflater.inflate(R.layout.custom_toast, (ViewGroup) this.mActivity.findViewById(R.id.toast_layout_root));
 	     mToastView.setBackgroundColor(Color.parseColor("#00ffffff"));
 	     mRootLayout =  (LinearLayout) mToastView.findViewById(R.id.toast_layout);
 	     mRootLayout.setBackgroundColor(Color.parseColor("#00ffffff"));
 	     mMessageTextView = (TextView) mToastView.findViewById(R.id.toastText);
-	     mToastImage =  (ImageView) mToastView.findViewById(R.id.toastImage);
-	     pattern = Pattern.compile(HEX_PATTERN);
-	     
-	     
+	     pattern = Pattern.compile(HEX_PATTERN);   
 	}
     
     @SuppressLint("ResourceAsColor")
@@ -72,10 +77,13 @@ public class ToastIt
 		}
 
 		 this.mActivity = activity;
+		 toastHelper = new ToastHelper(this.mActivity);
+		 t = new Toast(this.mActivity);
 		 
+		 //--Check to see if we are using "custom_font_layout" versus the regular "custom_toast"  
 		 if(useFontAwesome)
 		 {
-			//--re-intialize here to test font icon--
+ 			 //--uses one of the bootstrap icons as its image--
 			 mLayoutInflater = (LayoutInflater) this.mActivity.getLayoutInflater();
 		     mToastView = mLayoutInflater.inflate(R.layout.custom_font_layout, (ViewGroup) this.mActivity.findViewById(R.id.toast_layout_root));
 		     mToastView.setBackgroundColor(Color.parseColor("#00ffffff"));
@@ -87,8 +95,9 @@ public class ToastIt
 		 }
 		 else
 		 {
+			//--uses whatever image is passed to it--
 			 mLayoutInflater = (LayoutInflater) this.mActivity.getLayoutInflater();
-		     mToastView = mLayoutInflater.inflate(R.layout.custom_toast, (ViewGroup) this.mActivity.findViewById(R.id.toast_layout_root));
+		     mToastView = mLayoutInflater.inflate(R.layout.custom_image_layout, (ViewGroup) this.mActivity.findViewById(R.id.toast_layout_root));
 		     mToastView.setBackgroundColor(Color.parseColor("#00ffffff"));
 		     mRootLayout =  (LinearLayout) mToastView.findViewById(R.id.toast_layout);
 		     mRootLayout.setBackgroundColor(Color.parseColor("#00ffffff"));
@@ -99,6 +108,155 @@ public class ToastIt
 
 	}
 	
+    
+    /**
+     * Various basic show methods that are available to call if not passing an image or icon to use:
+     */
+    public void show(String message, int style)
+ 	{
+ 		mMessageTextView.setText(message);
+ 		
+ 		switch(style)
+       	{
+       		case 1: mRootLayout.setBackgroundResource(R.color.stt_holo_red);
+       			break;
+       		case 2: mRootLayout.setBackgroundResource(R.color.stt_holo_green);
+       			break;
+       		case 3: mRootLayout.setBackgroundResource(R.color.blue);
+       			break;
+       	}
+
+ 		 t.setView(mToastView);
+ 		 t.setGravity(Gravity.CENTER_VERTICAL, 100, 100);
+ 		 t.show();
+ 	}
+    
+ 	public void show(String message, int style, int length)
+ 	{
+ 		mMessageTextView.setText(message);
+ 		
+ 		switch(style)
+       	{
+       		case 1: mRootLayout.setBackgroundResource(R.color.stt_holo_red);
+       			break;
+       		case 2: mRootLayout.setBackgroundResource(R.color.stt_holo_green);
+       			break;
+       		case 3: mRootLayout.setBackgroundResource(R.color.blue);
+       			break;
+       	}
+
+ 		 toastHelper.setDuration(length);
+ 		 toastHelper.setView(mToastView);
+ 		 toastHelper.setGravity(Gravity.CENTER_VERTICAL, 100, 100);
+ 		 toastHelper.show();
+ 	}
+ 	
+ 	public void show(String message, String textColor, String hexCode)
+	{
+		mMessageTextView.setText(message);
+
+		if(validate(hexCode)){
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor)){
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+		}
+
+	    t.setView(mToastView);
+	    t.show();
+	}
+ 	
+ 	public void show(String message, Typeface font, String textColor, String hexCode)
+	{
+		mMessageTextView.setText(message);
+		
+		if(font != null){mMessageTextView.setTypeface(font);}
+
+		if(validate(hexCode)){
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor)){
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+		}
+		
+	    t.setView(mToastView);
+	    t.show();
+	}
+ 		
+ 	public void show(String message, String textColor, String hexCode, int length)
+	{
+		mMessageTextView.setText(message);
+
+		if(validate(hexCode)){
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor)){
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+		}
+		
+	    toastHelper.setDuration(length);
+	    toastHelper.setView(mToastView);
+	    toastHelper.show();
+	}
+ 	
+ 	public void show(String message, Typeface font, String textColor, String hexCode, int length)
+	{
+		mMessageTextView.setText(message);
+		
+		if(font != null){mMessageTextView.setTypeface(font);}
+
+		if(validate(hexCode)){
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor)){
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+		}
+		
+		toastHelper.setDuration(length);
+		toastHelper.setView(mToastView);
+		toastHelper.show();
+	}
+	
+ 
+    /**
+    * Various show methods that are available to call if passing an image to be used:
+    */
 	public void show(Drawable icon, String message, int style, int length)
 	{
 		mToastImage.setBackground(icon);
@@ -114,12 +272,10 @@ public class ToastIt
       			break;
       	}
 
-		ToastHelper t = new ToastHelper(this.mActivity);
-	    t.setDuration(length);
-	    t.setView(mToastView);
-	    t.setGravity(Gravity.CENTER_VERTICAL, 100, 100);
-	    t.show();
-		
+		 toastHelper.setDuration(length);
+		 toastHelper.setView(mToastView);
+		 toastHelper.setGravity(Gravity.CENTER_VERTICAL, 100, 100);
+		 toastHelper.show();
 	}
 	
 	public void show(Drawable icon, String message, String hexCode, int length)
@@ -135,11 +291,9 @@ public class ToastIt
 			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
 		}
 		
-		ToastHelper t = new ToastHelper(this.mActivity);
-	    t.setDuration(length);
-	    t.setView(mToastView);
-	    t.show();
-		
+		toastHelper.setDuration(length);
+		toastHelper.setView(mToastView);
+		toastHelper.show();
 	}
 	
 	public void show(Drawable icon, String message, Typeface font, String textColor, String hexCode)
@@ -166,23 +320,21 @@ public class ToastIt
 			mMessageTextView.setText("Hex code not valid, please try another");
 			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
 		}
-		
-		Toast t = new Toast(this.mActivity);
+
 	    t.setView(mToastView);
 	    t.show();
 	}
 	
 	public void show(Drawable icon, String message, Typeface font, String textColor, String hexCode, int length)
 	{
-		Animation animation = AnimationUtils.loadAnimation(this.mActivity, R.anim.anim);
+		//Animation animation = AnimationUtils.loadAnimation(this.mActivity, R.anim.anim);
 		
 		mToastImage.setBackground(icon);
 		mMessageTextView.setText(message);
+		
 		if(font != null){mMessageTextView.setTypeface(font);}
-		
-		
-		if(validate(hexCode))
-		{
+
+		if(validate(hexCode)){
 			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
 		}
 		else{
@@ -190,30 +342,54 @@ public class ToastIt
 			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
 		}
 		
-		if(validate(textColor))
-		{
+		if(validate(textColor)){
 			mMessageTextView.setTextColor(Color.parseColor(textColor));
 		}
 		else{
 			mMessageTextView.setText("Hex code not valid, please try another");
 			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
 		}
-		mRootLayout.setVisibility(View.INVISIBLE);
-		Toast t = new Toast(this.mActivity);
-	    t.setDuration(length);
+		
+		//mRootLayout.setVisibility(View.INVISIBLE);
+	    toastHelper.setDuration(length);
+	    toastHelper.setView(mToastView);
+	    toastHelper.show();
+	    //mRootLayout.setVisibility(View.VISIBLE);
+	    //mRootLayout.startAnimation(animation);
+		
+	}
+	
+	public void show(Bitmap img, String message, String textColor, String hexCode)
+	{
+		mToastImage.setImageBitmap(img);
+		mMessageTextView.setText(message);
+		
+		if(validate(hexCode)){
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor)){
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+		}
+
 	    t.setView(mToastView);
 	    t.show();
-	    mRootLayout.setVisibility(View.VISIBLE);
-	    mRootLayout.startAnimation(animation);
-		
 	}
 	
 	public void show(Bitmap img, String message, String textColor, String hexCode, int length)
 	{
 		mToastImage.setImageBitmap(img);
 		mMessageTextView.setText(message);
-		if(validate(hexCode))
-		{
+		
+		if(validate(hexCode)){
 			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
 		}
 		else{
@@ -221,21 +397,24 @@ public class ToastIt
 			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
 		}
 		
-		if(validate(textColor))
-		{
+		if(validate(textColor)){
 			mMessageTextView.setTextColor(Color.parseColor(textColor));
 		}
 		else{
 			mMessageTextView.setText("Hex code not valid, please try another");
 			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
 		}
-	
-		Toast t = new Toast(this.mActivity);
-	    t.setDuration(length);
-	    t.setView(mToastView);
-	    t.show();
+
+		toastHelper.setDuration(length);
+		toastHelper.setView(mToastView);
+		toastHelper.show();
 	}
 	
+	
+	/**
+    * Various show methods that are available to call if using one of the pre-defined bootstrap icons:
+    */
+	/*
 	public void show(String icon, String message, String hexCode, int length)
 	{		
 		mToastIcon.setIcon(icon);
@@ -253,6 +432,37 @@ public class ToastIt
 	    t.setDuration(length);
 	    t.setView(mToastView);
 	    t.show();
+	}*/
+	
+	public void show(String icon, String message, String textColor, String hexCode)
+	{
+		mToastIcon.setIcon(icon);
+		mMessageTextView.setText(message);
+		
+		if(validate(hexCode))
+		{
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor))
+		{
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+			mToastIcon.setTextColor(Color.parseColor(textColor));
+		}
+		else
+		{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+			mToastIcon.setTextColor(Color.parseColor("#ffffff"));
+		}
+
+	    t.setView(mToastView);
+	    t.show();
+	   
 	}
 	
 	public void show(String icon, String message, String textColor, String hexCode, int length)
@@ -281,10 +491,74 @@ public class ToastIt
 			mToastIcon.setTextColor(Color.parseColor("#ffffff"));
 		}
 
-		Toast t = new Toast(this.mActivity);
-	    t.setDuration(length);
+	    toastHelper.setDuration(length);
+	    toastHelper.setView(mToastView);
+	    toastHelper.show();
+	   
+	}
+	
+	public void show(String icon, String message, Typeface font, String textColor, String hexCode)
+	{
+		mToastIcon.setIcon(icon);
+		mMessageTextView.setText(message);
+		if(font != null){mMessageTextView.setTypeface(font);}
+		
+		if(validate(hexCode))
+		{
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor))
+		{
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+			mToastIcon.setTextColor(Color.parseColor(textColor));
+		}
+		else
+		{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+			mToastIcon.setTextColor(Color.parseColor("#ffffff"));
+		}
+
 	    t.setView(mToastView);
 	    t.show();
+	   
+	}
+	
+	public void show(String icon, String message, Typeface font, String textColor, String hexCode, int length)
+	{
+		mToastIcon.setIcon(icon);
+		mMessageTextView.setText(message);
+		if(font != null){mMessageTextView.setTypeface(font);}
+		
+		if(validate(hexCode))
+		{
+			mRootLayout.setBackgroundColor(Color.parseColor(hexCode));
+		}
+		else{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+		
+		if(validate(textColor))
+		{
+			mMessageTextView.setTextColor(Color.parseColor(textColor));
+			mToastIcon.setTextColor(Color.parseColor(textColor));
+		}
+		else
+		{
+			mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+			mToastIcon.setTextColor(Color.parseColor("#ffffff"));
+		}
+
+	    toastHelper.setDuration(length);
+	    toastHelper.setView(mToastView);
+	    toastHelper.show();
 	   
 	}
 	
@@ -354,21 +628,19 @@ public class ToastIt
 	
 	
 	 /**
-	   * Validate hex with regular expression
-	   * @param hex hex for validation
-	   * @return true valid hex, false invalid hex
-	   */
-	   public boolean validate(final String hex){
-	 
+     * Validate hex with regular expression
+     * @param hex hex for validation
+     * @return true valid hex, false invalid hex
+     */
+	 public boolean validate(final String hex){
 		  matcher = pattern.matcher(hex);
 		  return matcher.matches();
+	 }
 	 
-	   }
-	   
-	   public enum Speeds {
-		    FAST,
-		    MEDIUM,
-		    SLOW
-		  }
+	 public enum Speeds {
+	    FAST,
+	    MEDIUM,
+	    SLOW
+	 }
 
 }
