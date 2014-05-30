46bfada2ccf743372f2571340ccd6d773df03d27
Chad King <cking24343@gmail.com>
2014-05-30 15:09:07 -0400

    Updates
    -Added a 'master' show() method
    -Added reusable variables with get and set methods
    -Introduced textSize, and fontIconAnimations for 'master' show()

--- src/king/chad/toaster/ToastIt.java
+++ src/king/chad/toaster/ToastIt.java
@@ -45,9 +45,122 @@ public class ToastIt
     private static Typeface font;
     private static Map<String, String> faMap;
     private static final String HEX_PATTERN = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+    
+    //--Reusable variables with generic values--
+    private Drawable toasterDrawable = null;
+    private Bitmap toasterBitmap = null;
+    private Typeface toasterFont = null;
+    private int toasterDuration = -1;
+    private int toasterTextSize = -1;
+    private boolean toasterFlashIcon = false;
+    private String toasterFontIcon = null;
+    private String toasterMessage = "ToastIt!";
+    private String toasterTextColor = "#ffffff";
+    private String toasterBGColor = "#000000";
+    private Speeds toasterAnimationSpeed = null;
+    private Rotations toasterRotation = null;
+
+    
+	//--Getters--
+    public Drawable getToasterDrawable(){
+    	return toasterDrawable;
+    }
+    public Bitmap getToasterBitmap(){
+    	return toasterBitmap;
+    }
+	public Typeface getToasterFont(){
+    	return toasterFont;
+    }
+	public int getToasterDuration(){
+    	return toasterDuration;
+    }
+	public int getToasterTextSize(){
+    	return toasterTextSize;
+    }
+	public boolean getToasterFlashIcon(){
+    	return toasterFlashIcon;
+    }
+    public String getToasterFontIcon(){
+    	return toasterFontIcon;
+    }
+    public String getToasterTextColor(){
+    	return toasterTextColor;
+    }
+    public String getToasterBGColor(){
+    	return toasterBGColor;
+    }
+    public String getToasterMessage(){
+    	return toasterMessage;
+    }
+    public Speeds getToasterAnimationSpeed(){
+    	return toasterAnimationSpeed;
+    }
+    public Rotations getToasterRotation(){
+    	return toasterRotation;
+    }
+    
+    
+    //--Setters--
+    public void setToasterDrawable(Drawable temp){
+    	toasterDrawable = temp;
+    }
+    public void setToasterBitmap(Bitmap temp){
+    	toasterBitmap = temp;
+    }
+	public void setToasterFont(Typeface temp){
+    	toasterFont = temp;
+    }
+	public void setToasterDuration(int temp){
+    	toasterDuration = temp;
+    }
+	public void setToasterTextSize(int temp){
+		toasterTextSize = temp;
+    }
+	public void setToasterFlashIcon(boolean temp){
+    	toasterFlashIcon = temp;
+    }
+	public void setToasterFontIcon(String temp){
+    	toasterFontIcon = temp;
+    }
+	public void setToasterTextColor(String temp){
+    	toasterTextColor = temp;
+    }
+	public void setToasterBGColor(String temp){
+    	toasterBGColor = temp;
+    }
+    public void setToasterMessage(String txt){
+    	toasterMessage = txt;
+    }
+    public void setToasterAnimationSpeed(Speeds temp){
+    	toasterAnimationSpeed = temp;
+    }
+    public void setToasterRotation(Rotations temp){
+    	toasterRotation = temp;
+    }
 
     /**
-	* Various toaster constructors to use:
+     * This allows for the developer to reset the values back to the original state
+     * */
+	public void clearValues()
+	{
+		setToasterDrawable(null);
+		setToasterBitmap(null);
+		setToasterFont(null);
+		setToasterDuration(-1);
+		setToasterTextSize(-1);
+		setToasterFlashIcon(false);
+		setToasterFontIcon(null);
+		setToasterTextColor("#ffffff");
+		setToasterBGColor("#000000");
+		setToasterMessage("ToastIt!");
+		setToasterAnimationSpeed(null);
+		setToasterRotation(null);
+	}
+    
+    /**
+	* Basic constructor:
+	* 
+	* @param calling activity 
 	*/
     @SuppressLint("ResourceAsColor")
 	public ToastIt(Activity activity)
@@ -69,6 +182,12 @@ public class ToastIt
 	     pattern = Pattern.compile(HEX_PATTERN);   
 	}
     
+    /**
+   	* Basic constructor with images:
+   	* 
+   	* @param calling activity
+   	* @param boolean use image or icon
+   	*/
     @SuppressLint("ResourceAsColor")
 	public ToastIt(Activity activity, boolean useFontAwesome)
 	{
@@ -108,6 +227,145 @@ public class ToastIt
 
 	}
 	
+    
+    /**
+     * If you are using a reusable Toaster object and do not want to call the 
+     * various one time show methods below trying to figure out what to pass to them
+     * you can simply make a call to just 'show()'. This 'show()' will work with 
+     * only the variables that you initialized with the setter methods.  
+     */
+    public void show()
+    {
+	   	 /**ToastHelper is a modified class that allows for the time duration to be adjusted. 
+	   	  * If you declare a duration length the ToastHelper class will be called over the Toast class
+	   	  * when the view gets displayed.  
+	   	  */
+	   	 boolean useToasterHelper = false;
+	   	 
+	   	 //--Need to check for null values before assigning--
+	   	 if(getToasterDrawable() != null){
+	   		 try{
+	   			 mToastImage.setBackground(getToasterDrawable());
+	   		 }catch(Exception ex){}
+	   	 }
+	   	 if(getToasterBitmap() != null){
+	   		 try{
+	   			 mToastImage.setImageBitmap(getToasterBitmap());
+	   		 }catch(Exception ex){}
+	   	 }
+	   	 if(getToasterFont() != null){
+	   		 try{
+	   			 mMessageTextView.setTypeface(getToasterFont());
+	   		 }catch(Exception ex){}
+	   	 }
+	   	 if(getToasterDuration() > -1){
+	   		 try{
+	   			 useToasterHelper= true;
+	   			 toastHelper.setDuration(getToasterDuration());
+	   		 }catch(Exception ex){}
+	   	 }
+	   	 if(getToasterTextSize() > -1){
+	   		 try{
+	   			 mMessageTextView.setTextSize(getToasterTextSize());
+	   		 }catch(Exception ex){}
+	   	 }
+	   	 if(getToasterFlashIcon()){
+	   		 try{
+	   			 
+	   			 if(getToasterAnimationSpeed() != null){
+	   	    		 try{
+	   	    			
+	   	    				switch(getToasterAnimationSpeed())
+	   	    				{
+	   	    					case FAST: mToastIcon.startFlashing(this.mActivity, true,  FontAwesomeText.AnimationSpeed.FAST);
+	   	    						break;
+	   	    					case SLOW: mToastIcon.startFlashing(this.mActivity, true,  FontAwesomeText.AnimationSpeed.SLOW);
+	   	    						break;
+	   	    					default : mToastIcon.startFlashing(this.mActivity, true,  FontAwesomeText.AnimationSpeed.MEDIUM);
+	   	    						break;
+	   	    				}
+	   	    		 }catch(Exception ex){}
+	   	    	 }
+	   			 else //--Medium speed by default--
+	   				 mToastIcon.startFlashing(this.mActivity, true, FontAwesomeText.AnimationSpeed.MEDIUM);
+	   		 }catch(Exception ex){}
+	   	 }
+	   	 if(getToasterRotation() != null){
+	   		 try{
+	   			 if(getToasterRotation() == Rotations.Clockwise){
+	   				 if(getToasterAnimationSpeed() != null){
+		   				try{
+		   	    				switch(getToasterAnimationSpeed())
+		   	    				{
+		   	    					case FAST: mToastIcon.startRotate(this.mActivity, true, FontAwesomeText.AnimationSpeed.FAST);
+		   	    						break;
+		   	    					case SLOW: mToastIcon.startRotate(this.mActivity, true, FontAwesomeText.AnimationSpeed.SLOW);
+		   	    						break;
+		   	    					default : mToastIcon.startRotate(this.mActivity, true, FontAwesomeText.AnimationSpeed.MEDIUM);
+		   	    						break;
+		   	    				}
+		   	    		 }catch(Exception ex){}
+	   				 }
+	   				 else
+	   					mToastIcon.startRotate(this.mActivity, true, AnimationSpeed.MEDIUM);
+	   				}else if(getToasterRotation() == Rotations.CounterClockwise){
+	   					if(getToasterAnimationSpeed() != null){
+	   						try{
+		   	    				switch(getToasterAnimationSpeed())
+		   	    				{
+		   	    					case FAST: mToastIcon.startRotate(this.mActivity, false, FontAwesomeText.AnimationSpeed.FAST);
+		   	    						break;
+		   	    					case SLOW: mToastIcon.startRotate(this.mActivity, false, FontAwesomeText.AnimationSpeed.SLOW);
+		   	    						break;
+		   	    					default : mToastIcon.startRotate(this.mActivity, false, FontAwesomeText.AnimationSpeed.MEDIUM);
+		   	    						break;
+		   	    				}
+		   	    		 }catch(Exception ex){}
+	      				 }
+	      				 else
+	   					mToastIcon.startRotate(this.mActivity, false,  AnimationSpeed.MEDIUM);
+	   				}
+	   		 }catch(Exception ex){}
+	   	 } 
+	   	 if(getToasterFontIcon() != null){
+	   		 try{
+	   			 mToastIcon.setIcon(getToasterFontIcon());
+	   		 }catch(Exception ex){}
+	   	 } 
+	   	if(validate(getToasterTextColor())){
+	  		 try{
+	  			mMessageTextView.setTextColor(Color.parseColor(getToasterTextColor()));
+				mToastIcon.setTextColor(Color.parseColor(getToasterTextColor()));
+	  		 }catch(Exception ex){}
+	  	}
+	   	else{
+	   		mMessageTextView.setText("Hex code not valid, please try another");
+			mMessageTextView.setTextColor(Color.parseColor("#ffffff"));
+			mToastIcon.setTextColor(Color.parseColor("#ffffff"));
+		}
+	   	if(validate(getToasterBGColor())){
+	 		 try{
+	 			mRootLayout.setBackgroundColor(Color.parseColor(getToasterBGColor()));
+	 		 }catch(Exception ex){}
+	 	}
+	  	else{
+	  		mMessageTextView.setText("Hex code not valid, please try another");
+			mRootLayout.setBackgroundColor(Color.parseColor("#444444"));
+		}
+	   	 
+	
+	   	 if(useToasterHelper)
+	   	 {
+	   		 toastHelper.setView(mToastView);
+	   		 toastHelper.show();
+	   	 }
+	   	 else
+	   	 {
+	   		 t.setView(mToastView);
+	   		 t.show();
+	   	 }
+
+    }
     
     /**
      * Various basic show methods that are available to call if not passing an image or icon to use:
@@ -464,7 +722,7 @@ public class ToastIt
 	    t.show();
 	   
 	}
-	
+
 	public void show(String icon, String message, String textColor, String hexCode, int length)
 	{
 		mToastIcon.setIcon(icon);
@@ -562,7 +820,7 @@ public class ToastIt
 	   
 	}
 	
-	public void show(String icon, String message, Typeface font, String textColor, String hexCode, int length, boolean flashIcon, boolean rotateIconClockWise, boolean rotateIconCClockWise, String animationSpeed)
+	public void show(String icon, String message, Typeface font, String textColor, String hexCode, int length, boolean flashIcon, boolean rotateIconClockWise, boolean rotateIconCClockWise, String animationSpeed) 
 	{
 		mToastIcon.setIcon(icon);
 		mMessageTextView.setText(message);
@@ -627,6 +885,7 @@ public class ToastIt
 	}
 	
 	
+	
 	 /**
      * Validate hex with regular expression
      * @param hex hex for validation
@@ -637,10 +896,19 @@ public class ToastIt
 		  return matcher.matches();
 	 }
 	 
+	 
+	 /**
+	  * Using enums for various options for a given property
+	  */
 	 public enum Speeds {
-	    FAST,
-	    MEDIUM,
-	    SLOW
-	 }
+		    FAST,
+		    MEDIUM,
+		    SLOW
+		 }
+	 
+	 public enum Rotations {
+		    Clockwise,
+		    CounterClockwise
+		 }
 
 }
